e6799469001fdb930693021ef66b3fe4142f714b
Sai Chandorkar <saichandorkar06@.com>
2026-01-07 21:36:05 +0530

    fixx sub

--- yourtube/src/pages/subscription.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import SubscriptionPage from '@/components/SubscriptionPage';
-
-export default function Subscription() {
-  return <SubscriptionPage />;
-}
--- yourtube/src/pages/subscriptions.tsx
+++ yourtube/src/pages/subscriptions.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import SubscriptionPage from '@/components/SubscriptionPage';
+import { GetServerSideProps } from "next";
+
 
 export default function Subscriptions() {
   return <SubscriptionPage />;
 }
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: {}, // Client-side handles user history data
+  };
+};
