b3a52c49d6b22c6fce9aea754ca8c4aa1dc1ee85
Sai Chandorkar <saichandorkar06@.com>
2026-01-11 23:42:17 +0530

    fixx plauyer

--- yourtube/src/components/Videopplayer.tsx
+++ yourtube/src/components/Videopplayer.tsx
@@ -1272,22 +1272,50 @@ export default function CompleteVideoPlayer({
                   </button>
 
                   <div
-                    className={`hidden md:flex items-center transition-all ${
-                      showVolumeSlider ? "w-24 opacity-100" : "w-0 opacity-0"
+                    className={`hidden md:flex items-center transition-all duration-300 ${
+                      showVolumeSlider ? "w-28 opacity-100" : "w-0 opacity-0"
                     }`}
                   >
-                    <input
-                      type="range"
-                      min="0"
-                      max="1"
-                      step="0.01"
-                      value={volume}
-                      onChange={handleVolumeChange}
-                      className="w-full h-1 bg-white/30 rounded-full appearance-none cursor-pointer
-                    [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-3 [&::-webkit-slider-thumb]:h-3 
-                    [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white
-                    [&::-webkit-slider-thumb]:cursor-pointer [&::-webkit-slider-thumb]:hover:scale-110"
-                    />
+                    <div className="relative w-full h-1 bg-white/20 rounded-full overflow-hidden">
+                      {/* Volume fill indicator */}
+                      <div
+                        className="absolute left-0 top-0 h-full bg-gradient-to-r from-white to-white/90 rounded-full transition-all duration-150"
+                        style={{ width: `${volume * 100}%` }}
+                      />
+
+                      {/* Actual range input */}
+                      <input
+                        type="range"
+                        min="0"
+                        max="1"
+                        step="0.01"
+                        value={volume}
+                        onChange={handleVolumeChange}
+                        className="absolute inset-0 w-full h-full appearance-none bg-transparent cursor-pointer z-10
+                        [&::-webkit-slider-thumb]:appearance-none 
+                        [&::-webkit-slider-thumb]:w-3.5 
+                        [&::-webkit-slider-thumb]:h-3.5 
+                        [&::-webkit-slider-thumb]:rounded-full 
+                        [&::-webkit-slider-thumb]:bg-white
+                        [&::-webkit-slider-thumb]:cursor-pointer 
+                        [&::-webkit-slider-thumb]:shadow-lg
+                        [&::-webkit-slider-thumb]:transition-transform
+                        [&::-webkit-slider-thumb]:duration-150
+                        [&::-webkit-slider-thumb]:hover:scale-125
+                        [&::-webkit-slider-thumb]:active:scale-110
+                        [&::-moz-range-thumb]:w-3.5 
+                        [&::-moz-range-thumb]:h-3.5 
+                        [&::-moz-range-thumb]:rounded-full 
+                        [&::-moz-range-thumb]:bg-white
+                        [&::-moz-range-thumb]:border-0
+                        [&::-moz-range-thumb]:cursor-pointer 
+                        [&::-moz-range-thumb]:shadow-lg
+                        [&::-moz-range-thumb]:transition-transform
+                        [&::-moz-range-thumb]:duration-150
+                        [&::-moz-range-thumb]:hover:scale-125
+                        [&::-moz-range-thumb]:active:scale-110"
+                      />
+                    </div>
                   </div>
                 </div>
 
@@ -1295,7 +1323,6 @@ export default function CompleteVideoPlayer({
                   {formatTime(currentTime)} / {formatTime(duration)}
                 </div>
               </div>
-              ## Part 9: Right Controls and Settings Menu ```typescript
               {/* Right Controls */}
               <div
                 className="flex items-center gap-2 md:gap-3"
