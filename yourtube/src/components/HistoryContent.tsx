4602354548054d0f8d61c38d295cad1a65b8c9d9
Sai Chandorkar <saichandorkar06@.com>
2026-01-16 12:40:42 +0530

    fixx his

--- yourtube/src/components/HistoryContent.tsx
+++ yourtube/src/components/HistoryContent.tsx
@@ -533,7 +533,7 @@ export default function HistoryContent() {
                           <Link key={item._id} href={`/shorts/${short._id}`}>
                             <div className="flex-shrink-0 group cursor-pointer w-[160px] md:w-[180px]">
                               {/* FIXED: Added proper background and border for dark mode visibility */}
-                              <div className="short-thumbnail-container aspect-[9/16] rounded-xl overflow-hidden relative bg-gray-100 dark:bg-gray-800 border-2 border-gray-300 dark:border-gray-500 shadow-sm dark:shadow-gray-700">
+                              <div className="short-thumbnail-container aspect-[9/16] rounded-xl overflow-hidden relative bg-gray-200 dark:bg-gray-700 border-2 border-gray-300 dark:border-gray-500 shadow-md dark:shadow-xl">
                                 {thumbnailUrl && !thumbnailFailed ? (
                                   <img
                                     src={thumbnailUrl}
@@ -654,7 +654,7 @@ export default function HistoryContent() {
                                   className="flex-shrink-0"
                                 >
                                   {/* FIXED: Improved video thumbnail container */}
-                                  <div className="video-thumbnail-container w-[140px] h-[78px] md:w-[246px] md:h-[138px] bg-gray-100 dark:bg-gray-800 rounded-lg overflow-hidden relative border-2 border-gray-300 dark:border-gray-500 shadow-sm dark:shadow-gray-700">
+                                  <div className="video-thumbnail-container w-[140px] h-[78px] md:w-[246px] md:h-[138px] bg-gray-200 dark:bg-gray-700 rounded-lg overflow-hidden relative border-2 border-gray-300 dark:border-gray-500 shadow-md dark:shadow-xl">
                                     <video
                                       src={getVideoUrl(video)}
                                       className="w-full h-full object-cover bg-transparent"
